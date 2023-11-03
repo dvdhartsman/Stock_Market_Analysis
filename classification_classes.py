693f3195fe0fada3a70dfca0d1a95f3a08efce73
David Hartsman <dvdhartsman@gmail.com>
2023-11-03 16:24:29 -0400

    New class .py file

---
+++ classification_classes.py
@@ -0,0 +1,160 @@
+
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, recall_score, precision_score, f1_score, \
+log_loss, auc, roc_auc_score, roc_curve
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# Creating the class that will store my data 
+
+class Model():
+    model_list = []
+    model_df = pd.DataFrame(columns=['name','train_accuracy','train_prec','train_recall','train_f1','train_logloss',\
+                                     'test_accuracy','test_prec','test_recall','test_f1','test_logloss', "AUC"])
+    
+    def __init__(self, name, model, X_train, X_test, y_train, y_test, threshold=.5):
+        self.name = name
+        self.model = model
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.threshold = threshold
+        
+        # Collection of training attributes
+        # f1, recall, precision add "_macro" for multi-class
+        self.train_results = cross_validate(self.model, self.X_train, self.y_train, scoring=[
+            'precision', 'accuracy', 'recall', 'f1', 'neg_log_loss'], n_jobs=4, verbose=1)
+        # Train metrics
+        self.train_acc = np.mean(self.train_results['test_accuracy'])
+        self.train_prec = np.mean(self.train_results['test_precision']) # add "_macro" for multi-class
+        self.train_rec = np.mean(self.train_results['test_recall'])  # add "_macro" for multi-class
+        self.train_f1 = np.mean(self.train_results['test_f1'])  # add "_macro" for multi-class
+        self.train_logloss = -np.mean(self.train_results['test_neg_log_loss'])
+        
+        # Test metrics
+        self.y_pred_proba = self.model.predict_proba(self.X_test)
+        
+        # Thresholds!!! 
+        if self.threshold == .5:
+            self.y_pred = self.model.predict(self.X_test)
+        
+        else:
+            self.y_pred = (self.model.predict_proba(self.X_test)[:, 1] >= self.threshold).astype(int)
+        
+        self.test_score = model.score(self.X_test, self.y_test)
+        # average = "macro" for multi-class
+        self.test_recall = recall_score(self.y_test, self.y_pred, average='binary', zero_division=0)
+        # average = "macro" for multi-class
+        self.test_prec = precision_score(self.y_test, self.y_pred, average='binary', zero_division=0)
+        self.test_log_loss = log_loss(self.y_test, self.y_pred_proba)
+        # average = "macro" for multi-class
+        self.test_f1 = f1_score(self.y_test, self.y_pred, average='binary', zero_division=0)
+        
+        # AUC metrics -> Remove when we get to multi-class
+        self.auc = roc_auc_score(self.y_test, self.y_pred_proba[:,1])
+        
+        # Add model object to the class data container for access within the notebook
+        Model.model_list.append(self)
+        
+        # Dictionary containing all of the metrics to add to the dataframe
+        self.attributes = {'name':self.name, 'train_accuracy':self.train_acc, "train_prec": self.train_prec,
+                           "train_recall": self.train_rec, "train_f1": self.train_f1, \
+                           "train_logloss": self.train_logloss, \
+                          'test_accuracy':self.test_score, "test_prec": self.test_prec,
+                           "test_recall": self.test_recall, "test_f1": self.test_f1, \
+                           "test_logloss": self.test_log_loss, "AUC":self.auc}
+        
+        # Add the metrics to the class dataframe
+        Model.model_df.loc[len(Model.model_df)] = self.attributes
+        
+        
+        
+    # Roc Curve plot method -> needs to be removed for multi-class
+    def roc_curve(self):
+        # Create the plot
+        sns.set_style("dark")
+        fig, ax = plt.subplots(figsize=(6,6))
+        # get the predict_proba values
+        y_hat_hd = self.y_pred_proba[:, 1]
+
+        # Get the FPR and TPR data
+        fpr, tpr, thresholds = roc_curve(y_test, y_hat_hd)
+        # Plot the actual graph
+        ax.plot(fpr, tpr, label=f"{self.name} | AUC: {self.auc:.2f})")
+        # Y-axis
+        ax.set_yticks([0,.2,.4,.6,.8,1])
+        ax.set_yticklabels([0,20,40,60,80,100])
+        ax.set_ylabel("True Positive %")
+        # X-axis
+        ax.set_xticks([0,.2,.4,.6,.8,1])
+        ax.set_xticklabels([0,20,40,60,80,100])
+        ax.set_xlabel("False Positive %")
+        ax.set_title(f"{self.name} ROC Curve", fontsize=20)
+        plt.grid(False);
+        
+        
+    def compare_roc_curve(self):
+        sns.set_style("dark")
+        # Color Palette
+        colors = sns.color_palette("Paired", n_colors=30)
+        # Create the plot
+        fig, ax = plt.subplots(figsize=(7,7))
+        for i in range(len(Model.model_list)):
+            # get the predict_proba values
+            y_hat_hd = Model.model_list[i].y_pred_proba[:, 1]
+
+            # Get the FPR and TPR data
+            fpr, tpr, thresholds = roc_curve(Model.model_list[i].y_test, y_hat_hd)
+            # Plot the actual graph
+            ax.plot(fpr, tpr, color=colors[i], label=f'{Model.model_list[i].name} | AUC: {Model.model_list[i].auc:.2f})')
+        
+        ax.set_title(f"Comparison of ROC Curve")
+        ax.set_xlabel('False Positive Rate')
+        ax.set_ylabel('True Positive Rate')
+        ax.legend(loc='lower right')
+        plt.grid(False);
+    
+    
+    def confusion_matrix(self):
+        sns.set_style('white')
+        # Confusion Matrix Plot
+        fig, ax = plt.subplots(figsize=(6,6))
+        self.cm = ConfusionMatrixDisplay.from_estimator(\
+                                self.model, X=self.X_test, y=self.y_test, ax=ax)
+        plt.title(f'Confusion Matrix for "{self.name}" Test Data');
+    
+    
+    def __str__(self):
+      return f"Model name: {self.name}"
+
+
+# Class for collecting coefficients of models based on economic features
+
+class Importance():
+    df = pd.DataFrame(columns=['Interest Rates', 'GDP', 'ATR', 'lowest_bollinger_band', 'MACD', \
+                                    'lower_bollinger_band', 'upper_bollinger_band', '20_day_SMA', \
+                                    'one_up_bollinger_band', 'Close', 'High', 'Adj Close', 'Low', 'RSI', \
+                                    'Open', 'dollar_volume(M)', 'Volume', 'garman_klass_vol', 'SMB', 'CMA', 'HML',\
+                                    'RMW', 'Mkt-RF'])
+    
+    # Init method
+    def __init__(self, coefs):
+        self.dict = coefs
+    
+        # Add the metrics to the class dataframe
+        idx = len(Importance.df)
+        Importance.df.loc[idx] = self.dict
+    
+    
+    def add_coefs(self, coefs):
+        idx = len(Importance.df)
+        Importance.df.loc[idx] = coefs
+        
+    
+    def __str__(self):
+      return "Feature Importance/Coefficient DataFrame"
+        
