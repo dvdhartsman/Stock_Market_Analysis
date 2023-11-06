ca5e2369ceb93ef9810ac7f275d23426b4ce5ecd
David Hartsman <dvdhartsman@gmail.com>
2023-11-06 16:01:34 -0500

    Finished SPDR Notebook

---
+++ inferencial_functions.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+def plot_test_accuracy(df, sector_name:str, num_include=-1):
+    fig, ax = plt.subplots(figsize=(10,7))
+    palette = sns.color_palette("flare", n_colors=10)
+    data = df[['name', 'test_accuracy']].sort_values(by="test_accuracy", ascending=False)[0:num_include]
+    sns.barplot(data=data, y='name', x="test_accuracy", palette=palette)
+    ax.set_xlabel("Test Accuracy")
+    ax.set_ylabel("Model Name")
+    ax.set_title(f"Performance of Models - {sector_name}")
+    ax.set_xticks(np.arange(0,1,.1))
+    ax.set_xticklabels(np.arange(0,100,10))
+    plt.grid(False);
+    
+    
+    
+feature_importances = pd.read_csv('/Users/samalainabayeva/Desktop/Capstone Project/Feature_Importances.csv')
+feature_importances.rename(columns={"Unnamed: 0": "Model Target"}, inplace=True)
+feature_importances.set_index("Model Target", inplace=True)
+    
+    
+    
+def print_feature_importances(start, stop, interval=1, feats=5, data=feature_importances):
+    for i in range(start, stop, interval):
+        print("*" *40)
+        print(f"Top {feats} Feature Importance(s):")
+        print(feature_importances.iloc[i].sort_values(ascending=False)[:feats])
+        
+        
+
+def describe_predictions(model, ticker, df, cat=1):
+    targets = df[df["Ticker"] == ticker][['One_Month_Change', 'Three_Month_Change', 'Six_Month_Change', \
+                                         'Twelve_Month_Change']]
+
+    combined_df = model.X_test.join(targets)
+    predictions = list(model.y_pred)
+    combined_df["Y_PREDS"] = predictions
+
+    return combined_df[combined_df["Y_PREDS"] == cat][['One_Month_Change', 'Three_Month_Change', 'Six_Month_Change',\
+                                                     'Twelve_Month_Change']].describe()
+
+
+def show_time_horizon(df, time_horizon):
+    time_dict = {"One":"1-Month", "Three":"3-Month", "Six":'6-Month', "Twelve":'12-Month'}
+    return df[df['name'].str.contains(str(time_dict[time_horizon]))].sort_values(by="test_accuracy", ascending=False)
